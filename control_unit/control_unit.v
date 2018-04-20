6ce52f57a08a0b99fba221bd7cc3ed88eec82915
MIYAGI RYOTA <kuryugujo9240@gmail.com>
2018-04-20 14:13:11 +0900

    edit control_unit

--- control_unit/control_unit.v
+++ control_unit/control_unit.v
@@ -1,15 +1,14 @@
 module control_unit (
 	input clock, reset, exec,
 	input [3:0] phase,
-	output counter_reset,
+	output register_reset,
 	output reg p1, p2, p3, p4, p5);
 	
 	reg running = 0;
 
-	assign counter_reset = reset;
-	
+	assign register_reset = reset;
 
-	always @(posedge exec or posedge reset) begin
+	always @(posedge exec or negedge reset) begin
 		
 		//start and stop running on exec pushed	
 		if (exec == 1'b1) begin 
